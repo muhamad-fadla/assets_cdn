325a535651e0b6b30ca74742c713ead3fd958331
muhamad-fadla <yuratamma@gmail.com>
2021-10-10 15:54:23 +0700

    update: new file

---
+++ functions/server.js
@@ -0,0 +1,17 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+
+app.use(express.static(path.join(__dirname, '../static')));
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
